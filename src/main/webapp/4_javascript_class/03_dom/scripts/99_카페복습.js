ef1ce80a6cf2e147ddb92d6ebcebee30f7d2cd11
gkwow <gkwow@DESKTOP-INAQOMK>
2022-02-24 17:31:47 +0900

    WEBUI

--- src/main/webapp/4_javascript_class/03_dom/scripts/99_카페복습.js
+++ src/main/webapp/4_javascript_class/03_dom/scripts/99_카페복습.js
@@ -1,20 +1,16 @@
-let item = document.getElementsByClassName('item')
 
-item.onclick = function(e) {
+window.onload = function() {
+   var item = document.getElementsByClassName('item');
    
    for(var i=0; i<item.length; i++){
       item[i].onclick = function(e){
          alert('ok');
          var price = e.target.getAttribute('data-price');
          alert(price);
-         
-         
          }
       }
-	
 
 }
-	
 
 
 
